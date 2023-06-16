7a87aa2797dd94fb25ec9f5995c86a7be5862817
ash1505-lesha <ashleshamahajan766@gmail.com>
2023-06-17 01:30:51 +0530

    Add Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        RWOnly first = new RWOnly();
+//        first.name="Ash";
+
+        first.setName("Ash");
+        first.getName();
+    }
 }
